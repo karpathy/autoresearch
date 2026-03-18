c46353494b5669bc326fa25510f2eecbd0295753
Ian Patterson <ian@tunameltsmyheart.com>
2026-03-18 10:17:53 -0400

    exp: increase max_depth from 9 to 10 (scaling mitigates drawdown risk)

--- train.py
+++ train.py
@@ -268,7 +268,7 @@ def main():
 
     model = ExtraTreesRegressor(
         n_estimators=2000,
-        max_depth=9,
+        max_depth=10,
         min_samples_leaf=400,
         random_state=42,
         n_jobs=-1,
