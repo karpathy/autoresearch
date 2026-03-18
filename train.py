5dbe89661d7879172e62f42bd5e2a28c14788e6c
Ian Patterson <ian@tunameltsmyheart.com>
2026-03-18 08:17:35 -0400

    exp: reduce max_depth from 6 to 4

--- train.py
+++ train.py
@@ -233,7 +233,7 @@ def main():
 
     model = ExtraTreesRegressor(
         n_estimators=200,
-        max_depth=6,
+        max_depth=4,
         min_samples_leaf=500,
         random_state=42,
         n_jobs=-1,
